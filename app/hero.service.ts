df3f6fb37cf82a5766c4eda283d1ab518c759795
lancelynyrd <you@example.com>
2016-08-11 18:25:03 +0800

    Initial+QuickStart+Tutorial

---
+++ app/hero.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
+
+@Injectable()
+export class HeroService {
+  getHeroes() {
+    return Promise.resolve(HEROES);
+  }
+  getHeroesSlowly() {
+    return new Promise<Hero[]>(resolve =>
+      setTimeout(() => resolve(HEROES), 5000) // 2 seconds
+    );
+  } 
+  getHero(id: number) {
+  return this.getHeroes()
+             .then(heroes => heroes.find(hero => hero.id === id));
+  } 
+}
